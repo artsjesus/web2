687515bdb470a5136aee78de3a9774c2d997ec37
Konstantin Maurin <artsjesus7@gmail.com>
2024-09-08 19:49:15 +0300

    fix

--- catalog/models.py
+++ catalog/models.py
@@ -19,16 +19,15 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to="catalog/image", blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория", blank=True, null=True, related_name="Products")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория", blank=True, null=True)
     price = models.FloatField(verbose_name="Цена за покупку")
     created_at = models.DateTimeField(verbose_name="Дата создания")
     updated_at = models.DateTimeField(verbose_name="Дата последнего изменения")
 
-
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
-        ordering = ["category", ["name"]]
+        ordering = ["category", "name"]
 
     def __str__(self):
         return self.name
