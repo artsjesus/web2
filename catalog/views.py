6d8e9d86587af393ac470537c70a0201d5b26c06
Konstantin Maurin <artsjesus7@gmail.com>
2024-09-22 17:26:08 +0300

    CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
+from django.views.generic import ListView, DetailView, TemplateView
+
+
 # Create your views here.
 
-
-def products_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "products_list.html", context)
+class ProductListView(ListView):
+    model = Product
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        print(f"{name} ({phone}): {message}")
-
-    return render(request, "contacts.html")
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, "product_detail.html", context)
+class ContactsTemplateView(TemplateView):
+    template_name = "catalog/contacts.html"
